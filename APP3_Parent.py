eeac05e7a29e0915d0e4774bcfa4532170b82a2b
Moussaoui noah <128749614+NLK-Noah@users.noreply.github.com>
2023-11-24 15:57:20 +0100

    Update APP3_Parent.py

--- APP3_Parent.py
+++ APP3_Parent.py
@@ -2,66 +2,50 @@ from microbit import *
 import music
 import time
 
-# Adresse de la mémoire flash pour stocker la valeur de count
-FLASH_ADDRESS = 0
 
-def write_count_to_flash(count):
-    try:
-        with open("count", "w") as file:
-            file.write(str(count))
-    except:
-        pass
-# ici c est pour stocker ma variable pour quand on l'étains 
-def read_count_from_flash():
-    try:
-        with open("count", "r") as file:
-            return int(file.read())
-    except:
-        return 0
+MILK_COUNT = 0
+
 
 def start():
     for x in range(2):
         music.play(["C4:4", "D4", "E4", "C4"])
     for x in range(2):
         music.play(["E4:4", "F4", "G4:8"])
-        time.sleep(5)
 
 def command():
     if button_a.was_pressed():
         setting()
+    
 
 def menu():
     while True:
         display.show(Image.DUCK)
-        if command():
-            break
+        if button_a.was_pressed() : 
+            setting()
+
 
 def setting():
-    global count
-
-    display.scroll("Launch of the milk counter")
-    display.show(count)
+    global MILK_COUNT
+    display.scroll(" milk")
+    display.show(MILK_COUNT)
     while True:
         if button_b.is_pressed():
-            count += 1
-            display.show(count) #cc
+            MILK_COUNT += 1
+            display.show(MILK_COUNT)
             sleep(500)
         if button_a.is_pressed():
-            count -= 1
-            display.show(count)
+            MILK_COUNT -= 1
+            display.show(MILK_COUNT)
             sleep(500)
         if accelerometer.was_gesture('shake'):
-            count = 0
-            display.show(count)
+            MILK_COUNT = 0
+            display.show(MILK_COUNT)
             sleep(500)
         if pin_logo.is_touched():
             # Sauvegarde la valeur das  la mémoire flash
-            write_count_to_flash(count)
             menu()
 
 def main():
-    global count
-    count = read_count_from_flash()
     start()
     menu()
 
