7f84039584cd0107df65df0434d225e2ddf4702d
Moussaoui noah <128749614+NLK-Noah@users.noreply.github.com>
2023-12-11 16:55:48 +0100

    Update BebiEnfant_Backend.py

--- BebiEnfant_Backend.py
+++ BebiEnfant_Backend.py
@@ -6,8 +6,7 @@ import random
 radio.config(group=23)
 radio.on()
 
-key = "Betag"
-key = "Betag"
+KEY = "Betag"
 dictionnary = {}
 lst = ['00','01','02','03']
 for element in lst : 
@@ -58,13 +57,14 @@ def menu():
         pré: start ()
         post: affiche une image de menu et donne accès à tous les commande possible
     """
+    global KEY
     global MILK_COUNT
     while True:
-        veilleuse(key)
+        veilleuse(KEY)
 	radio.receive()
         message_recu = radio.receive()
         if message_recu : 
-            unpack_data(message_recu,key)
+            unpack_data(message_recu,KEY)
         elif button_a.was_pressed() : 
             display.scroll(MILK_COUNT)
             
@@ -98,19 +98,20 @@ def setting():
 
 
 
-def vigenere(message, key, decryption=False):
+def vigenere(message, KEY, decryption=False):
+    global KEY
     text = ""
-    key_length = len(key)
-    key_as_int = [ord(k) for k in key]
+    KEY_length = len(kKEY)
+    KEY_as_int = [ord(k) for k in KEY]
 
     for i, char in enumerate(str(message)): #le i fait réf à la position du str et le char fait réf ay caractère de la position 
-        key_index = i % key_length
+        KEY_index = i % KEY_length
         #Letters encryption/decryption
         if char.isalpha():
             if decryption:
-                modified_char = chr((ord(char.upper()) - key_as_int[key_index] + 26) % 26 + ord('A'))
+                modified_char = chr((ord(char.upper()) - KEY_as_int[KEY_index] + 26) % 26 + ord('A'))
             else : 
-                modified_char = chr((ord(char.upper()) + key_as_int[key_index] - 26) % 26 + ord('A'))
+                modified_char = chr((ord(char.upper()) + KEY_as_int[KEY_index] - 26) % 26 + ord('A'))
             #Put back in lower case if it was
             #La fonction ord() permet de renvoyer la valeur unicode associé au caractère
             #la fonction chr() fait l'inverse càd qu'elle renvoie le caractère associé à la valeur unicode
@@ -120,16 +121,17 @@ def vigenere(message, key, decryption=False):
         #Digits encryption/decryption
         elif char.isdigit():
             if decryption:
-                modified_char = str((int(char) - key_as_int[key_index]) % 10)
+                modified_char = str((int(char) - KEY_as_int[KEY_index]) % 10)
             else:
-                modified_char = str((int(char) + key_as_int[key_index]) % 10)
+                modified_char = str((int(char) + KEY_as_int[KEY_index]) % 10)
             text += modified_char
         else:
             text += char
     return text
 
 
-def unpack_data (encrypted_packed,key) : 
+def unpack_data (encrypted_packed,KEY) : 
+    global KEY
     global dictionnary
     global MILK_COUNT
     decryption_message = encrypted_packed.split('|')
@@ -139,56 +141,56 @@ def unpack_data (encrypted_packed,key) :
     if decryption_message[0] == '00' :
         for clef in dictionnary : 
             if clef == '00' :
-                nonce_decrypted = vigenere(nonce,key) 
+                nonce_decrypted = vigenere(nonce,KEY) 
                 if nonce_decrypted in dictionnary['00'] : 
                     display.scroll('ERROR message already received')
                 else : 
                     dictionnary['00'].append(nonce_decrypted)
-                    message_decripte_vigenere = vigenere(content,key,True) #Here we will decrypt the content of the message
+                    message_decripte_vigenere = vigenere(content,KEY,True) #Here we will decrypt the content of the message
                     response_challenge = calculate_challenge(message_decripte_vigenere)
                     valeur_hashing_response = hashing(str(response_challenge))
-                    send_packet("00",valeur_hashing_response,key)
-                    key += str(response_challenge)
+                    send_packet("00",valeur_hashing_response,KEY)
+                    KEY += str(response_challenge)
                     return message_decripte_vigenere
                 
             if clef == '01' : #MILK_COUNT
-                nonce_decrypted = vigenere(nonce,key) 
+                nonce_decrypted = vigenere(nonce,KEY) 
                 if nonce_decrypted in dictionnary['01'] : 
                     display.scroll('ERROR message already received')
                 else : 
                     dictionnary['01'].append(nonce_decrypted)
                     display.scroll('Message added')
-                    message_decripte_vigenere = vigenere(content,key,True)
+                    message_decripte_vigenere = vigenere(content,KEY,True)
 		    MILK_COUNT = message_decripte_vigenere 
                     return message_decripte_vigenere
                     
             if clef == '02' : 
-                nonce_decrypted = vigenere(nonce,key) 
+                nonce_decrypted = vigenere(nonce,KEY) 
                 if nonce_decrypted in dictionnary['02'] : 
                     display.scroll('ERROR message already received')
                 else : 
                     dictionnary['02'].append(nonce_decrypted)
                     display.scroll('Message added')
-                    message_decripte_vigenere = vigenere(content,key,True) #Here we will decrypt the content of the message
+                    message_decripte_vigenere = vigenere(content,KEY,True) #Here we will decrypt the content of the message
                     return message_decripte_vigenere
             if clef == '03' :
-                nonce_decrypted = vigenere(nonce,key) 
+                nonce_decrypted = vigenere(nonce,KEY) 
                 if nonce_decrypted in dictionnary['03'] : 
                     display.scroll('ERROR message already received')
                 else : 
                     dictionnary['03'].append(nonce_decrypted)
                     display.scroll('Message added')
-                    message_decripte_vigenere = vigenere(content,key,True) #Here we will decrypt the content of the message
+                    message_decripte_vigenere = vigenere(content,KEY,True) #Here we will decrypt the content of the message
                     return message_decripte_vigenere
 
 
 def calculate_challenge (challenge) : 
     return int(challenge)*5
 
-def send_packet(type_message,contenu,key): 
-    contenu_vigenered = vigenere(contenu,key)
+def send_packet(type_message,contenu,KEY): 
+    contenu_vigenered = vigenere(contenu,KEY)
     nbre_aleatoire = random.randrange(5000)
-    nbre_aleatoire_vigenered = vigenere(nbre_aleatoire,key)
+    nbre_aleatoire_vigenered = vigenere(nbre_aleatoire,KEY)
     encrypted_message = nbre_aleatoire_vigenered + ':' + contenu_vigenered
     long_message = len(encrypted_message)
     radio_send = '{0}|{1}|{2}'.format(type_message,str(long_message),encrypted_message)
@@ -196,7 +198,7 @@ def send_packet(type_message,contenu,key):
     radio.send(radio_send)
 
 
-def veilleuse (key) : 
+def veilleuse (KEY) : 
     """
     @Pre : Doit recevoir une commande de la part du BeTag parent qui lui dit que le bébé a été mis au lit
            Si elle reçoit un 'start_veilleuse' elle doit rester allumer tant que la lumière n'est pas 
@@ -214,18 +216,18 @@ def veilleuse (key) :
         "99999:"
         "99999:"
         "99999"))
-        temperature_measurement(key)
-        agitation_bebe(key)
+        temperature_measurement(KEY)
+        agitation_bebe(KEY)
             
     elif 50 > light_level <= 90 :
         """
         Il faudra vérifier une luminosité qui empêche les bébés de dormir
         """
 
-        temperature_measurement(key)
-        agitation_bebe(key)
+        temperature_measurement(KEY)
+        agitation_bebe(KEY)
 
-def temperature_measurement (key,type='02') : 
+def temperature_measurement (KEY,type='02') : 
     """
     Cette fonctionnalité pourrait être intéressante dans la fonctionnalité de mesure de luminosité 
     car c'est pendant que le bébé dort que la température est le paramètre important à surveiller
@@ -237,13 +239,13 @@ def temperature_measurement (key,type='02') :
     """
     measure_temperature = temperature()
     if measure_temperature <= 19 :
-        send_packet(key, type, str(measure_temperature)+'- Need heat') #Utilisation def send_packet pour envoie
+        send_packet(KEY, type, str(measure_temperature)+'- Need heat') #Utilisation def send_packet pour envoie
         #de données cryptées 
     elif measure_temperature >= 25 :
-        send_packet(key, type, str(measure_temperature)+'- Need cold') #Utilisation def send_packet pour envoie
+        send_packet(KEY, type, str(measure_temperature)+'- Need cold') #Utilisation def send_packet pour envoie
         #de données cryptées
 
-def calibration(key,type='03'):
+def calibration(KEY,type='03'):
     display.scroll(compass.heading())
     compass.calibrate()
     if compass.is_calibrated() == True : 
@@ -252,23 +254,23 @@ def calibration(key,type='03'):
         orientation2 = compass.heading()
         gap = abs(orientation2-orientation1)
         if gap <= 15:
-            send_packet(type,'Endormie',key)
+            send_packet(type,'Endormie',KEY)
         elif 15 < gap <= 45:
-            send_packet(type,'Agite',key)
+            send_packet(type,'Agite',KEY)
         else:
-            send_packet(type,'Tres agite',key)
+            send_packet(type,'Tres agite',KEY)
         orientation3 = compass.heading()
         if orientation3 == 180: 
-            send_packet(type,'Changer la position',key)
+            send_packet(type,'Changer la position',KEY)
     else : 
         compass.clear_calibration()
 
-def agitation_bebe (key,type='03') : 
+def agitation_bebe (KEY,type='03') : 
     noise_level = microphone.sound_level()
     if 45 < noise_level < 60: 
-        send_packet(type,'Bebe peut etre reveille et agite',key)
+        send_packet(type,'Bebe peut etre reveille et agite',KEY)
     elif noise_level > 60 : 
-        send_packet(type,'Bebe reveille',key)
+        send_packet(type,'Bebe reveille',KEY)
 
         
 display.scroll('Welcome')
